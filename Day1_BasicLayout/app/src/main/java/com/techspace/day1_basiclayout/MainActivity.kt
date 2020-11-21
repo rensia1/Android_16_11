b99f00ad7cb876524490b070b6c27b70beac7c05
Techbear <techbearHub@users.noreply.github.com>
2020-11-21 14:08:59 +0200

    day 2

---
+++ Day1_BasicLayout/app/src/main/java/com/techspace/day1_basiclayout/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.techspace.day1_basiclayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnChangeName = findViewById<Button>(R.id.btnChangeName)
+        val inputName = findViewById<EditText>(R.id.inputName)
+        val lblName = findViewById<TextView>(R.id.lblName)
+
+        btnChangeName.setOnClickListener{
+
+            var name = inputName.text
+            //var a = "Sveiki, " + name
+            //divas slipsvitras ir komentars
+
+            //lai paraditos Toast zina
+            Toast.makeText(this@MainActivity, "Sveiki, " + name, Toast.LENGTH_SHORT).show()
+
+            //Lai nomainitu tekstu
+            lblName.text = name
+        }
+    }
+
+}
